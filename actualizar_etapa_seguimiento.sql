f2ae8e3d71ae3d229247c7f39138a477549b9832
David Caro <davidcaroo@users.noreply.github.com>
2026-03-29 19:19:18 -0500

    chore: remove old sql files, update .gitignore to exclude skills folder

--- actualizar_etapa_seguimiento.sql
+++
@@ -1,5 +0,0 @@
--- Modificar el enum de etapa_venta para añadir 'seguimiento'
-ALTER TABLE empresas MODIFY COLUMN etapa_venta enum('prospectado','contactado','negociacion','seguimiento','ganado','perdido') DEFAULT 'prospectado';
-
--- También actualizar la tabla de trazabilidad
-ALTER TABLE trazabilidad MODIFY COLUMN etapa_venta enum('prospectado','contactado','negociacion','seguimiento','ganado','perdido') DEFAULT NULL;
